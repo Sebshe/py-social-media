8eef5ceeedc013ea268e3ba0b1107617a30b2ecb
Petro Abramov <sebshe3@gmail.com>
2023-07-19 02:00:24 +0300

    Addded urls for user

--- py_social_media/urls.py
+++ py_social_media/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
